b0056caa4600194358432447f2121834a527932c
ghalia111 <>
2021-05-05 11:13:12 +0000

    demo

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+// 1- ask the user about favourite color 
+//show image on the answer 
+// choice red -> red pic
+// choice green -> green
+*/
+var userColor = prompt('that your favourite color')
+if (userColor === 'red'){
+  document.write('<img src="https://www.tarkett-asia.com/media/img/M/TH_3917011_3707003_3708011_3912011_3914011_800_800.jpg">') 
+}
+else if(userColor === 'green'){
+  document.write('<img src="https://www.dictionary.com/e/wp-content/uploads/2020/02/20200727_emoji_greenHeart_1000x700.png">')
+}
